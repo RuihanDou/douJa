707fd119e3fc850a4a17cf25c9c2c9cf265fd561
窦睿翰 <douruihandouruihan@outlook.com>
2021-03-08 09:50:25 +0800

    更新剑指offer

---
+++ src/main/java/local/begin/LeetCode/LeetCode056Solution.java
@@ -0,0 +1,72 @@
+package local.begin.LeetCode;
+
+import local.begin.tools.DebugTools;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 56. 合并区间
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 示例 2：
+ *
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 104
+ */
+
+
+public class LeetCode056Solution {
+
+    public int[][] merge(int[][] intervals) {
+
+        // 考虑为空的情况
+        if(intervals.length == 0){
+            return new int[0][2];
+        }
+
+        // 以左区间大小排序
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        List<int[]> merged = new ArrayList<>();
+
+        for (int i = 0; i < intervals.length; i++){
+            int L = intervals[i][0], R = intervals[i][1];
+
+            if(merged.size() < 1 || merged.get(merged.size() - 1)[1] < L){
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(R, merged.get(merged.size() - 1)[1]);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    public static void main(String[] args) {
+        DebugTools.print(Arrays.asList(new int[0][2]));
+    }
+
+}
