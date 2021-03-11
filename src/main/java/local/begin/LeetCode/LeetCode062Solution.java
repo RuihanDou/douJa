7589eb73b8289bf2fe52492ec370d4311daa8821
douruihan <douruihan@mafengwo.com>
2021-03-11 11:29:45 +0800

    更新LeetCode代码

--- src/main/java/local/begin/LeetCode/LeetCode062Solution.java
+++ src/main/java/local/begin/LeetCode/LeetCode062Solution.java
@@ -1,6 +1,8 @@
 package local.begin.LeetCode;
 
 
+import local.begin.tools.DebugTools;
+
 /**
  * 62. 不同路径
  * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
@@ -62,6 +64,10 @@ public class LeetCode062Solution {
         return checkerboard[m-1][n-1];
     }
 
-    //TODO：改成用滚动数组
+
+    public static void main(String[] args) {
+        LeetCode062Solution solution = new LeetCode062Solution();
+        DebugTools.print(solution.uniquePaths(3, 7));
+    }
 
 }
