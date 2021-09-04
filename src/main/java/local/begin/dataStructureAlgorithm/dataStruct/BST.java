d55bef710148b214f35f4156d553ced62b905a26
RuihanDou <ruihandou@outlook.com>
2021-09-05 02:07:53 +0800

    添加了二分搜索树的中序遍历，和后序遍历

--- src/main/java/local/begin/dataStructureAlgorithm/dataStruct/BST.java
+++ src/main/java/local/begin/dataStructureAlgorithm/dataStruct/BST.java
@@ -153,6 +153,37 @@ public class BST  <E extends Comparable<E>>{
         preOrder(node.right);
     }
 
+    // 二分搜索树的中序遍历
+    // 二分搜索树的中序遍历输出从小到大的排序
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    // 中序遍历以node为根的二分搜索树，递归算法
+    private void inOrder(Node node) {
+        if(node == null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    // 二分搜索树的后序遍历
+    public void postOrder(){
+        postOrder(root);
+    }
+
+    // 后序遍历以node为节点的二分搜索树，递归算法
+    private void postOrder(Node node){
+        if(node == null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
@@ -196,6 +227,10 @@ public class BST  <E extends Comparable<E>>{
         // 2  4     8  //
         /////////////////
         bst.preOrder();
+        System.out.println();
+        bst.inOrder();
+        System.out.println();
+        bst.postOrder();
         System.out.println();
         System.out.println(bst);
 
