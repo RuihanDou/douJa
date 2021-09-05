475cdd6b3a2b218f1d4b6450b63167654f577214
RuihanDou <ruihandou@outlook.com>
2021-09-05 15:33:37 +0800

    增加BST中删除元素的逻辑

--- src/main/java/local/begin/dataStructureAlgorithm/dataStruct/BST.java
+++ src/main/java/local/begin/dataStructureAlgorithm/dataStruct/BST.java
@@ -1,7 +1,6 @@
 package local.begin.dataStructureAlgorithm.dataStruct;
+import java.util.*;
 import java.util.LinkedList;
-import java.util.Stack;
-import java.util.Queue;
 
 /**
  * 二分搜索树 Binary Search Tree
@@ -222,6 +221,122 @@ public class BST  <E extends Comparable<E>>{
         }
     }
 
+    // 寻找二分搜索树的最小元素
+    public E minimum(){
+        if(size == 0) {
+            throw  new IllegalArgumentException("BT is empty!");
+        }
+        return minimum(root).e;
+    }
+
+    // 返回以node为根的二分搜索树的最小值所在的节点
+    private Node minimum(Node node){
+        if(node.left == null){
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    // 寻找二分搜索树的最大元素
+    public E maximum(){
+        if(size == 0){
+            throw new IllegalArgumentException("BST is empty!");
+        }
+        return maximum(root).e;
+    }
+
+    //返回以node为根的二分搜素树的最大值所在的节点
+    private Node maximum(Node node){
+        if(node.right == null){
+            return node;
+        }
+        return maximum(node.right);
+    }
+
+    // 从二分搜索树中删除最小值所在的节点，返回最小值
+    public E removeMin(){
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    // 删除以node为根的二分搜索树中的最小节点
+    // 返回删除节点后的新二分搜索树的根
+    private Node removeMin(Node node){
+        if(node.left == null){
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    // 从二分搜索树中删除最大值所在的节点，返回最大值
+    public E removeMax(){
+        E ret = maximum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    // 删除以node为根的二分搜素树中的最小节点
+    // 返回删除节点后的新二分搜索树的根
+    private Node removeMax(Node node){
+        if(node.right == null){
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    // 从二分搜索树中删除元素为e的节点
+    public void remove(E e){
+       root = remove(root, e);
+    }
+
+    // 删除以node为根的二分搜索树中值为e的节点，递归算法
+    // 返回删除后节点新的二分搜索树的根
+    private Node remove(Node node, E e) {
+        if(node == null){
+            return null;
+        }
+        if(e.compareTo(node.e) < 0){
+            node.left = remove(node.left, e);
+            return node;
+        } else if(e.compareTo(node.e) > 0){
+            node.right = remove(node.right, e);
+            return node;
+        } else { // e == node.e
+            // 待删除的节点左子树为空的情况
+            if(node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            // 待删除的节点右子树为空的情况
+            if(node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+            // 待删除的节点左右子树均不为空的情况
+            // 找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+            // 用这个节点顶替待删除节点的位置
+            Node successor = maximum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            return successor;
+        }
+
+    }
 
     @Override
     public String toString(){
@@ -254,30 +369,67 @@ public class BST  <E extends Comparable<E>>{
 
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
-        int[] nums = new int[]{5,3,6,8,4,2};
-        for (int num : nums){
-            bst.add(num);
+//        int[] nums = new int[]{5,3,6,8,4,2};
+//        for (int num : nums){
+//            bst.add(num);
+//        }
+
+//        /////////////////
+//        //      5      //
+//        //    /   \    //
+//        //   3    6    //
+//        //  / \    \   //
+//        // 2  4     8  //
+//        /////////////////
+//
+//        bst.preOrder();
+//        System.out.println();
+//        bst.preOrderNR();
+//        System.out.println();
+//        bst.inOrder();
+//        System.out.println();
+//        bst.postOrder();
+//        System.out.println();
+//        bst.levelOrder();
+//        System.out.println();
+//        System.out.println(bst);
+        int n = 1000;
+        Random random = new Random();
+        for(int i = 0; i < n; i++){
+            bst.add(random.nextInt(10000));
         }
 
-        /////////////////
-        //      5      //
-        //    /   \    //
-        //   3    6    //
-        //  / \    \   //
-        // 2  4     8  //
-        /////////////////
+        List<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty()){
+            nums.add(bst.removeMin());
+        }
 
-        bst.preOrder();
-        System.out.println();
-        bst.preOrderNR();
-        System.out.println();
-        bst.inOrder();
-        System.out.println();
-        bst.postOrder();
-        System.out.println();
-        bst.levelOrder();
-        System.out.println();
-        System.out.println(bst);
+        System.out.println(nums);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums.get(i - 1) > nums.get(i)){
+                throw new IllegalArgumentException("Error");
+            }
+        }
+        System.out.println("removeMin test complete");
+
+
+        for(int i = 0; i < n; i++){
+            bst.add(random.nextInt(10000));
+        }
+        nums.clear();
+        while (!bst.isEmpty()){
+            nums.add(bst.removeMax());
+        }
+
+        System.out.println(nums);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums.get(i - 1) < nums.get(i)){
+                throw new IllegalArgumentException("Error");
+            }
+        }
+        System.out.println("removeMax test complete");
+
+
     }
 
 }
