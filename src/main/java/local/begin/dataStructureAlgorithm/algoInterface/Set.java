645386a17b9504912ce848ff7faca744ddb85642
RuihanDou <ruihandou@outlook.com>
2021-09-05 16:15:10 +0800

    增加bst set

---
+++ src/main/java/local/begin/dataStructureAlgorithm/algoInterface/Set.java
@@ -0,0 +1,11 @@
+package local.begin.dataStructureAlgorithm.algoInterface;
+
+public interface Set<E> {
+
+    void add(E e);
+    void remove(E e);
+    boolean contains(E e);
+    int getSize();
+    boolean isEmpty();
+
+}
