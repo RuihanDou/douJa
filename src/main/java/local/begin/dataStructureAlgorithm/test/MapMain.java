3673b75d7371a044296cc3988207476e78954144
RuihanDou <ruihandou@outlook.com>
2021-09-05 18:59:38 +0800

    对比 BSTMap 和 LinkedListMap 两种映射的对比

---
+++ src/main/java/local/begin/dataStructureAlgorithm/test/MapMain.java
@@ -0,0 +1,52 @@
+package local.begin.dataStructureAlgorithm.test;
+
+import local.begin.dataStructureAlgorithm.algoInterface.Map;
+import local.begin.dataStructureAlgorithm.dataStruct.BSTMap;
+import local.begin.dataStructureAlgorithm.dataStruct.LinkedListMap;
+import local.begin.dataStructureAlgorithm.helper.FileOperation;
+
+import java.util.ArrayList;
+
+public class MapMain {
+    private static double testMap(Map<String, Integer> map, String filename){
+
+        long startTime = System.nanoTime();
+
+        System.out.println(filename);
+        ArrayList<String> words = new ArrayList<>();
+        if(FileOperation.readFile(filename, words)) {
+            System.out.println("Total words: " + words.size());
+
+            for (String word : words){
+                if(map.contains(word))
+                    map.set(word, map.get(word) + 1);
+                else
+                    map.add(word, 1);
+            }
+
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+
+        String filename = "pride-and-prejudice.txt";
+
+        BSTMap<String, Integer> bstMap = new BSTMap<>();
+        double time1 = testMap(bstMap, filename);
+        System.out.println("BST Map: " + time1 + " s");
+
+        System.out.println();
+
+        LinkedListMap<String, Integer> linkedListMap = new LinkedListMap<>();
+        double time2 = testMap(linkedListMap, filename);
+        System.out.println("Linked List Map: " + time2 + " s");
+
+    }
+}
