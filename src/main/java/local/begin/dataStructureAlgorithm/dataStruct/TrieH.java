c3d50eb2580cd56f7fae65b248f074213c67fd48
RuihanDou <RuihanDou@outlook.com>
2021-09-25 18:32:23 +0800

    增加使用哈希代替treemap的trie

---
+++ src/main/java/local/begin/dataStructureAlgorithm/dataStruct/TrieH.java
@@ -0,0 +1,77 @@
+package local.begin.dataStructureAlgorithm.dataStruct;
+
+import java.util.HashMap;
+
+public class TrieH {
+
+    private class Node{
+
+        public boolean isWord;
+        public HashMap<Character, Node> next = new HashMap<>();
+
+        public Node(boolean isWord){
+            this.isWord = isWord;
+        }
+
+        public Node(){
+            this.isWord = false;
+        }
+
+    }
+
+    private Node root;
+    private int size;
+
+    public TrieH(){
+        root = new Node();
+        size = 0;
+    }
+
+    // 获得Trie中存储的单词数量
+    public int getSize(){
+        return size;
+    }
+
+    // 向Trie中添加一个新的word
+    public void add(String word){
+        Node cur = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(cur.next.get(c) == null){
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+
+        if(!cur.isWord){
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    // 查询word是否在trie中
+    public boolean contains(String word){
+        Node cur = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(cur.next.get(c) == null){
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    // 查询是否在Trie中有单词以prefix为前缀
+    public boolean isPrefix(String prefix){
+        Node cur = root;
+        for(int i = 0; i < prefix.length(); i++){
+            char c = prefix.charAt(i);
+            if(cur.next.get(c) == null){
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+}
--- src/main/java/local/begin/dataStructureAlgorithm/test/TrieMain.java
+++ src/main/java/local/begin/dataStructureAlgorithm/test/TrieMain.java
@@ -2,6 +2,7 @@ package local.begin.dataStructureAlgorithm.test;
 
 import local.begin.dataStructureAlgorithm.dataStruct.BSTSet;
 import local.begin.dataStructureAlgorithm.dataStruct.Trie;
+import local.begin.dataStructureAlgorithm.dataStruct.TrieH;
 import local.begin.dataStructureAlgorithm.helper.FileOperation;
 
 import java.util.ArrayList;
@@ -17,11 +18,13 @@ public class TrieMain {
             long startTime = System.nanoTime();
 
             BSTSet<String> set = new BSTSet<>();
-            for(String word: words)
+            for(String word: words){
                 set.add(word);
+            }
 
-            for(String word: words)
+            for(String word: words){
                 set.contains(word);
+            }
 
             long endTime = System.nanoTime();
 
@@ -35,11 +38,13 @@ public class TrieMain {
             startTime = System.nanoTime();
 
             Trie trie = new Trie();
-            for(String word: words)
+            for(String word: words){
                 trie.add(word);
+            }
 
-            for(String word: words)
+            for(String word: words){
                 trie.contains(word);
+            }
 
             endTime = System.nanoTime();
 
@@ -47,6 +52,26 @@ public class TrieMain {
 
             System.out.println("Total different words: " + trie.getSize());
             System.out.println("Trie: " + time + " s");
+
+            // ---
+
+            startTime = System.nanoTime();
+
+            TrieH trieH = new TrieH();
+            for(String word: words){
+                trieH.add(word);
+            }
+
+            for(String word: words){
+                trieH.contains(word);
+            }
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + trieH.getSize());
+            System.out.println("TrieH: " + time + " s");
         }
     }
 
