1a8f94ecd31a2f454b94ba6879f10e0155943476
Ryan <ruihandou@outlook.com>
2022-02-05 00:15:27 +0800

    春节刷题第3天，初二

--- src/main/java/local/begin/leetCode/LeetCode0106Solution.java
+++ src/main/java/local/begin/leetCode/LeetCode0106Solution.java
@@ -1,6 +1,11 @@
 package local.begin.leetCode;
 
 
+import local.begin.struct.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 106. 从中序与后序遍历序列构造二叉树
  * 给定两个整数数组 inorder 和 postorder ，其中 inorder 是二叉树的中序遍历， postorder 是同一棵树的后序遍历，请你构造并返回这颗 二叉树 。
@@ -30,6 +35,37 @@ package local.begin.leetCode;
  */
 public class LeetCode0106Solution {
 
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int inLen = inorder.length;
+        int postLen = postorder.length;
+
+        if(inLen != postLen){
+            throw new RuntimeException("Incorrect input data");
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < inLen; i++){
+            map.put(inorder[i], i);
+        }
+
+        return buildTree(postorder, 0, postLen - 1, map, 0, inLen - 1);
+
+    }
+
+    private TreeNode buildTree(int[] postorder, int postLeft, int postRight, Map<Integer, Integer> map, int inLeft, int inRight){
+
+        if(postLeft > postRight || inLeft > inRight){
+            return null;
+        }
+
+        int rootVal = postorder[postRight];
+        TreeNode root = new TreeNode(rootVal);
+        int pIndex = map.get(rootVal);
+
+        root.left = buildTree(postorder, postLeft, postLeft + pIndex - inLeft - 1, map, inLeft, pIndex - 1);
+        root.right = buildTree(postorder, postRight - inRight + pIndex, postRight - 1, map, pIndex + 1, inRight);
+        return root;
+    }
 
 
 }
