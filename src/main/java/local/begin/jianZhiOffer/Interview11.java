34d15b4b61bb1cc48465679b148f7b5c40c12cc4
douruihan <douruihan@mafengwo.com>
2022-02-16 16:07:39 +0800

    leetcode 0154 1024 剑指 offer 07 11

---
+++ src/main/java/local/begin/jianZhiOffer/Interview11.java
@@ -0,0 +1,40 @@
+package local.begin.jianZhiOffer;
+
+
+/**
+ * 剑指 Offer 11. 旋转数组的最小数字
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ *
+ * 给你一个可能存在 重复 元素值的数组 numbers ，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为1。
+ *
+ * 示例 1：
+ *
+ * 输入：[3,4,5,1,2]
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：[2,2,2,0,1]
+ * 输出：0
+ * 注意：本题与主站 154 题相同：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/
+ */
+public class Interview11 {
+
+    public int minArray(int[] numbers) {
+
+        int l = 0, r = numbers.length - 1;
+
+        while (l < r){
+            int mid = l + (r - l) / 2;
+            if(numbers[mid] < numbers[r]){
+                r = mid;
+            }
+            else if (numbers[mid] > numbers[r]) {
+                l = mid + 1;
+            } else {
+                r--;
+            }
+        }
+        return numbers[l];
+    }
+
+}
