dd20c982bfa9b55d5d4a8fc6321b6b34dcc3c915
Ryan <ruihandou@outlook.com>
2022-02-22 15:48:37 +0800

    增加刷题89 92

---
+++ src/main/java/local/begin/leetCode/LeetCode0092Solution.java
@@ -0,0 +1,51 @@
+package local.begin.leetCode;
+
+import local.begin.struct.ListNode;
+
+
+/**
+ * 92. 反转链表 II
+ * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4,5], left = 2, right = 4
+ * 输出：[1,4,3,2,5]
+ * 示例 2：
+ *
+ * 输入：head = [5], left = 1, right = 1
+ * 输出：[5]
+ *
+ *
+ * 提示：
+ *
+ * 链表中节点数目为 n
+ * 1 <= n <= 500
+ * -500 <= Node.val <= 500
+ * 1 <= left <= right <= n
+ *
+ *
+ * 进阶： 你可以使用一趟扫描完成反转吗？
+ */
+public class LeetCode0092Solution {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode pre = dummy;
+        for(int i = 0; i < left - 1; i++){
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for(int i = 0; i < right - left; i++){
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummy.next;
+    }
+
+}
