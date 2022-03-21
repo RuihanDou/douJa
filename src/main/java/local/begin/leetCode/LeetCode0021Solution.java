a34df23965ec0a3c99aa42e28960c5c08d7e3249
douruihan <douruihan@mafengwo.com>
2022-03-21 14:13:41 +0800

    修改21

--- src/main/java/local/begin/leetCode/LeetCode0021Solution.java
+++ src/main/java/local/begin/leetCode/LeetCode0021Solution.java
@@ -27,42 +27,42 @@ import local.begin.struct.ListNode;
 
 public class LeetCode0021Solution {
 
-    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
-        ListNode dummyHead =  new ListNode(0);
-        ListNode cur = dummyHead;
-        while (null != l1 && null != l2) {
-            if(l1.val < l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-            } else {
-                cur.next = l2;
-                l2 = l2.next;
-            }
-            cur = cur.next;
+//    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+//        ListNode dummyHead =  new ListNode(0);
+//        ListNode cur = dummyHead;
+//        while (null != l1 && null != l2) {
+//            if(l1.val < l2.val) {
+//                cur.next = l1;
+//                l1 = l1.next;
+//            } else {
+//                cur.next = l2;
+//                l2 = l2.next;
+//            }
+//            cur = cur.next;
+//
+//        }
+//        if (null == l2) {
+//            cur.next = l1;
+//        } else {
+//            cur.next = l2;
+//        }
+//        return dummyHead.next;
+//    }
 
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(null == list1) {
+            return list2;
         }
-        if (null == l2) {
-            cur.next = l1;
+        if(null == list2) {
+            return list1;
+        }
+
+        if(list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next,list2);
+            return list1;
         } else {
-            cur.next = l2;
-        }
-        return dummyHead.next;
-    }
-
-    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
-        if(null == l1) {
-            return l2;
-        }
-        if(null == l2) {
-            return l1;
-        }
-
-        if(l1.val < l2.val) {
-            l1.next = mergeTwoLists2(l1.next,l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists2(l1, l2.next);
-            return l2;
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
     }
 
