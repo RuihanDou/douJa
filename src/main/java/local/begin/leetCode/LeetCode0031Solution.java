d1a72575aca6e91b2977305dc290904799737823
douruihan <douruihan@mafengwo.com>
2022-03-21 14:31:44 +0800

    修改31

--- src/main/java/local/begin/leetCode/LeetCode0031Solution.java
+++ src/main/java/local/begin/leetCode/LeetCode0031Solution.java
@@ -2,19 +2,63 @@ package local.begin.leetCode;
 
 public class LeetCode0031Solution {
 
+    /**
+     * 注意到下一个排列总是比当前排列要大，除非该排列已经是最大的排列。我们希望找到一种方法，能够找到一个大于当前序列的新序列，且变大的幅度尽可能小。具体地：
+     *
+     * 我们需要将一个左边的「较小数」与一个右边的「较大数」交换，以能够让当前排列变大，从而得到下一个排列。
+     *
+     * 同时我们要让这个「较小数」尽量靠右，而「较大数」尽可能小。当交换完成后，「较大数」右边的数需要按照升序重新排列。这样可以在保证新排列大于原来排列的情况下，使变大的幅度尽可能小。
+     *
+     * 以排列 [4,5,2,6,3,1][4,5,2,6,3,1] 为例：
+     *
+     * 我们能找到的符合条件的一对「较小数」与「较大数」的组合为 22 与 33，满足「较小数」尽量靠右，而「较大数」尽可能小。
+     *
+     * 当我们完成交换后排列变为 [4,5,3,6,2,1][4,5,3,6,2,1]，此时我们可以重排「较小数」右边的序列，序列变为 [4,5,3,1,2,6][4,5,3,1,2,6]。
+     *
+     * 具体地，我们这样描述该算法，对于长度为 nn 的排列 aa：
+     *
+     * 首先从后向前查找第一个顺序对 (i,i+1)(i,i+1)，满足 a[i] < a[i+1]a[i]<a[i+1]。这样「较小数」即为 a[i]a[i]。此时 [i+1,n)[i+1,n) 必然是下降序列。
+     *
+     * 如果找到了顺序对，那么在区间 [i+1,n)[i+1,n) 中从后向前查找第一个元素 jj 满足 a[i] < a[j]a[i]<a[j]。这样「较大数」即为 a[j]a[j]。
+     *
+     * 交换 a[i]a[i] 与 a[j]a[j]，此时可以证明区间 [i+1,n)[i+1,n) 必为降序。我们可以直接使用双指针反转区间 [i+1,n)[i+1,n) 使其变为升序，而无需对该区间进行排序。
+     *
+     * @param nums
+     */
+    // 4，5，2，6，3，1 -> 4，5，3，1，2，6
     public void nextPermutation(int[] nums) {
-        int len = nums.length;
-        if(len <= 1){
-            return;
+        // 下面 的while 循环和其初始化设置 i = nums.length - 2 是为了寻找最右边的一个逆序对
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
         }
-        boolean descend = true;
-        for(int i = 0; i < len - 1; i++){
-            if(nums[i] < nums[i + 1]){
-                descend = false;
+        // nums[i] = 2
+        // i >= 1 代表 nums 中存在这么一个逆序对
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
             }
+            // nums[j] = 3
+            swap(nums, i, j);
         }
+        // 4，5，3，6，2，1 元素3 i位置以后的元素反转
+        reverse(nums, i + 1);
+    }
 
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
     }
 
 }
