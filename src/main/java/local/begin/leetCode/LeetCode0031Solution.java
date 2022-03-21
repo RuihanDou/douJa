1cb750ffaa1f784bb5f2e5d59fa5e8c6aec27528
douruihan <douruihan@mafengwo.com>
2022-03-21 14:45:03 +0800

    修改31

--- src/main/java/local/begin/leetCode/LeetCode0031Solution.java
+++ src/main/java/local/begin/leetCode/LeetCode0031Solution.java
@@ -1,5 +1,7 @@
 package local.begin.leetCode;
 
+import java.util.Arrays;
+
 public class LeetCode0031Solution {
 
     /**
@@ -27,11 +29,15 @@ public class LeetCode0031Solution {
      */
     // 4，5，2，6，3，1 -> 4，5，3，1，2，6
     public void nextPermutation(int[] nums) {
-        // 下面 的while 循环和其初始化设置 i = nums.length - 2 是为了寻找最右边的一个逆序对
+
+        //第一步，寻找最右边的逆序对
+        // 下面 的while 循环和其初始化设置 i = nums.length - 2 是为了寻找最右边的一个逆序对(逆序对是指左小于右)
         int i = nums.length - 2;
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
+
+        //第二步，寻找逆序对，左边的元素位置i右边的  从数组最右边的比nums[i]大的元素， swap
         // nums[i] = 2
         // i >= 1 代表 nums 中存在这么一个逆序对
         if (i >= 0) {
@@ -42,6 +48,7 @@ public class LeetCode0031Solution {
             // nums[j] = 3
             swap(nums, i, j);
         }
+        //第三步，i+1位置之后的元素翻转
         // 4，5，3，6，2，1 元素3 i位置以后的元素反转
         reverse(nums, i + 1);
     }
@@ -59,6 +66,13 @@ public class LeetCode0031Solution {
             left++;
             right--;
         }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 2, 6, 3, 1};
+        LeetCode0031Solution solution = new LeetCode0031Solution();
+        solution.nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
 }
