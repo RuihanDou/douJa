8a7e7c8bfc106e0f4cba25a8da82eae55fddd228
douruihan <douruihan@mafengwo.com>
2022-03-21 14:19:44 +0800

    修改22

--- src/main/java/local/begin/leetCode/LeetCode0022Solution.java
+++ src/main/java/local/begin/leetCode/LeetCode0022Solution.java
@@ -31,25 +31,37 @@ public class LeetCode0022Solution {
         return rst;
     }
 
-    private void recursion(List<String> list, StringBuilder cur, int l, int r, int max){
+    /**
+     *
+     * 递归加回溯
+     *
+     * @param rstList
+     * @param cur
+     * @param l
+     * @param r
+     * @param max
+     */
+    private void recursion(List<String> rstList, StringBuilder cur, int l, int r, int max){
 
         // 递归成功且结束
         if(cur.length() == max * 2){
-            list.add(cur.toString());
+            rstList.add(cur.toString());
             return;
         }
 
         // 左括号数量只要小于最大值就可以添加
         if (l < max){
             cur.append('(');
-            recursion(list, cur, l+1, r, max);
+            recursion(rstList, cur, l+1, r, max);
+            // 本轮结束 要恢复状态，使该递归函数调用下，后面的代码能正常生效
             cur.deleteCharAt(cur.length() - 1);
         }
 
         // 右括号必须数量小于左括号才可以添加
         if (r < max && r < l){
             cur.append(')');
-            recursion(list, cur, l, r+1, max);
+            recursion(rstList, cur, l, r+1, max);
+            // 本轮结束 要恢复状态，使该递归函数调用下，后面的代码能正常生效
             cur.deleteCharAt(cur.length() - 1);
         }
     }
