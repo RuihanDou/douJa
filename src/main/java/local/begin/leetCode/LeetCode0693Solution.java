70aad54baf58005c5688fa8fb43802705a13bede
douruihan <douruihan@mafengwo.com>
2022-03-28 10:46:10 +0800

    增加693

---
+++ src/main/java/local/begin/leetCode/LeetCode0693Solution.java
@@ -0,0 +1,49 @@
+package local.begin.leetCode;
+
+
+/**
+ *693. 交替位二进制数
+ * 给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：n = 5
+ * 输出：true
+ * 解释：5 的二进制表示是：101
+ * 示例 2：
+ *
+ * 输入：n = 7
+ * 输出：false
+ * 解释：7 的二进制表示是：111.
+ * 示例 3：
+ *
+ * 输入：n = 11
+ * 输出：false
+ * 解释：11 的二进制表示是：1011.
+ *
+ *
+ * 提示：
+ *
+ * 1 <= n <= 231 - 1
+ */
+public class LeetCode0693Solution {
+
+    public boolean hasAlternatingBits(int n) {
+        int pre = -1;
+
+        while (n != 0){
+            int cur = n % 2;
+            if(cur == pre){
+                return false;
+            }
+            pre = cur;
+            n /= 2;
+        }
+
+        return true;
+
+    }
+
+}
