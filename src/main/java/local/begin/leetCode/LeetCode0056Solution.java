19b9a67111d5e1c64776fb2f86aac3119b2c10ea
douruihan <douruihan@mafengwo.com>
2022-04-11 15:50:55 +0800

    修改 56

--- src/main/java/local/begin/leetCode/LeetCode0056Solution.java
+++ src/main/java/local/begin/leetCode/LeetCode0056Solution.java
@@ -54,10 +54,12 @@ public class LeetCode0056Solution {
 
         for (int i = 0; i < intervals.length; i++){
             int L = intervals[i][0], R = intervals[i][1];
-
+            // 如果 结果集里为空，或者，结果集里的最右界 在目前区间的左界之前，证明要开启新的一段区间
             if(merged.size() < 1 || merged.get(merged.size() - 1)[1] < L){
                 merged.add(new int[]{L, R});
-            } else {
+            }
+            // 当前区间可以附着到结果集的最右区间上
+            else {
                 merged.get(merged.size() - 1)[1] = Math.max(R, merged.get(merged.size() - 1)[1]);
             }
         }
