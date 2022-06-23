57e2071ba4f789a36234ac23a71cc383805b86a7
Ryan <ruihandou@outlook.com>
2022-06-23 23:00:44 +0800

    修改 leetcode03

--- src/main/java/local/begin/leetCode/LeetCode0003Solution.java
+++ src/main/java/local/begin/leetCode/LeetCode0003Solution.java
@@ -42,7 +42,7 @@ public class LeetCode0003Solution {
         for(int l = 0; l < n; l++){
             // 因为循环伊始，左指针就向右移动一格，移除的字符对应从出现的字符中移除
             if(l != 0){
-                occ.remove(s.charAt(l));
+                occ.remove(s.charAt(l - 1));
             }
             // 右移 右边界 因为右移向右多看一格，所以 l < n - 1，向右移动得保证子串中没有重复字符
             while (r < n - 1 && !occ.contains(s.charAt(r + 1))){
